73385efaf39476d8dc912240d6b57d744dff0355
Md Abu Ahsan Basir <maab.career@gmail.com>
2018-04-24 20:03:33 +0600

    Create Routes for Admin

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+* Admin Routes
+*/
+
+Route::get('admin/home','AdminController@index');
+Route::get('admin/login','Admin\LoginController@showLoginForm')->name('admin.login');
+Route::post('admin/login','Admin\LoginController@login');
+Route::post('admin/logout','Admin\LoginController@logout')->name('admin.logout');
+Route::post('admin-password/email','Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::get('admin-password/reset','Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('admin-password/reset','Admin\ResetPasswordController@reset');
+Route::get('admin-password/reset/{token}','Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
+Route::get('admin/register','Admin\RegisterController@showRegistrationForm')->name('admin.register');
+Route::post('admin/register','Admin\RegisterController@register');
